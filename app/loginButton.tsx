1bb0a465b6f4ff3c8b04db5c6c13b67c0213b45b
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2026-07-31 08:57:19 +0100

    sign in working on local server, not localhost

--- app/loginButton.tsx
+++ app/loginButton.tsx
@@ -8,4 +8,6 @@ export function LoginButton() {
         return <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => signOut()}>Sign out</button>
     }
     return <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => signIn("spotify")}>Sign in with Spotify</button>
+
+
 }
